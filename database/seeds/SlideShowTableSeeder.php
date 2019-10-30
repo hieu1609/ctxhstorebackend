c8d9f26193b59a7d8034237fc0fcc697a082b5b0
Hieu Nguyen <16521675@gm.uit.edu.vn>
2019-10-30 22:07:49 +0700

    update api GK

--- database/seeds/SlideShowTableSeeder.php
+++ database/seeds/SlideShowTableSeeder.php
@@ -14,47 +14,41 @@ class SlideShowTableSeeder extends Seeder
     {
         $slideShows = [
             [
-                'image' => 'null',
-                'title' => 'null',
+                'image' => 'https://i.ibb.co/d5FhQqw/gmdn2019.jpg',
+                'title' => 'gmdn2019',
                 "created_at"=> "2019-08-28 03:12:33",
                 "updated_at"=> "2019-08-28 03:12:33"
             ],
             [
-                'image' => 'null',
-                'title' => 'null',
+                'image' => 'https://i.ibb.co/fnBGVTN/camon.jpg',
+                'title' => 'camon',
                 "created_at"=> "2019-08-28 03:12:34",
                 "updated_at"=> "2019-08-28 03:12:34"
 
             ],
             [
-                'image' => 'null',
-                'title' => 'null',
+                'image' => 'https://i.ibb.co/qm74xM4/tuyenthanhvien.jpg',
+                'title' => 'tuyenthanhvien',
                 "created_at"=> "2019-08-28 03:12:35",
                 "updated_at"=> "2019-08-28 03:12:35"
             ],
             [
-                'image' => 'null',
-                'title' => 'null',
-                "created_at"=> "2019-08-28 03:12:35",
-                "updated_at"=> "2019-08-28 03:12:35"
-            ],
-            [
-                'image' => 'null',
-                'title' => 'null',
-                "created_at"=> "2019-08-28 03:12:35",
-                "updated_at"=> "2019-08-28 03:12:35"
-            ],
-            [
-                'image' => 'null',
-                'title' => 'null',
-                "created_at"=> "2019-08-28 03:12:35",
-                "updated_at"=> "2019-08-28 03:12:35"
-            ],
-            [
-                'image' => 'null',
-                'title' => 'null',
+                'image' => 'https://i.ibb.co/K9ZT5r5/tramkyuc.jpg',
+                'title' => 'tramkyuc',
                 "created_at"=> "2019-08-28 03:12:36",
                 "updated_at"=> "2019-08-28 03:12:36"
+            ],
+            [
+                'image' => 'https://i.ibb.co/GWHmFbv/quengopsach.png',
+                'title' => 'quyengopsach',
+                "created_at"=> "2019-08-28 03:12:37",
+                "updated_at"=> "2019-08-28 03:12:37"
+            ],
+            [
+                'image' => 'https://i.ibb.co/fDHKPHM/cfs.jpg',
+                'title' => 'cfs',
+                "created_at"=> "2019-08-28 03:12:37",
+                "updated_at"=> "2019-08-28 03:12:37"
             ]
         ];
         
